a6327a91f176ef592ee39d179b803cb2b35b9833
Jose Somilleda <joseluissom004@gmail.com>
2024-01-17 20:51:47 -0600

    Fix relacioando al deeplink

--- app/src/main/java/com/jsomven/pokedex/ui/components/PokemonDetail.kt
+++ app/src/main/java/com/jsomven/pokedex/ui/components/PokemonDetail.kt
@@ -5,12 +5,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jsomven.pokedex.R
 import com.jsomven.pokedex.core.domain.model.EggGroups
 import com.jsomven.pokedex.core.domain.model.EvolutionChainResource
 import com.jsomven.pokedex.core.domain.model.PokemonSpecies
@@ -39,10 +45,16 @@ fun PokemonDetail(
             eggGroups = pokemon.eggGroups
         )
 
-        if (pokemon.id > 0){
+        if (pokemon.id > 0) {
             PokemonDetailsButtons(
                 onEvolutionChainClick = onEvolutionChainClick,
                 onAbilitiesClick = onAbilitiesClick
+            )
+        } else {
+            Spacer(modifier = Modifier.height(32.dp))
+            Text(
+                text = stringResource(id = R.string.pokemon_not_found),
+                fontSize = 24.sp
             )
         }
 
